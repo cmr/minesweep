8ed6422b229304a7520b0717d3a4267dc7e43958
Corey Richardson <corey@octayn.net>
2012-11-09 19:57:13 -0500

    Print memory usage information in benchmark

--- bench_grid.c
+++ bench_grid.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <limits.h>
+#include <malloc.h>
 
 #include "grid.h"
 
 int main(void) {
-	grid_t *grid = grid_new(4096, 4096, 4096 * 1024);
+	grid_t *grid = grid_new(4096, 4096, 4096 * 2048);
+	malloc_stats();
 	return grid->height;
 }
