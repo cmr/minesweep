28e1cfcdd9d2f4d38fc0a05a6614231fe4002c97
Corey Richardson <corey@octayn.net>
2012-11-11 20:24:27 -0500

    Remove improbable test case

--- test_grid.c
+++ test_grid.c
@@ -32,14 +32,21 @@ int main(void) {
 
 	/* Mother of all memory hogs. 2^64 * 4 bytes. Counting on linux to do some
 	 * sort of CoW or lazy allocation... we'll see */
-	grid = grid_new(UINT_MAX - 1, UINT_MAX - 1, 0);
-	grid_set(grid, coord(UINT_MAX - 2, UINT_MAX - 2), SQUARE_BOMB);
-	assert(square_is_bomb(grid_get(grid, coord(UINT_MAX - 2, UINT_MAX - 2))));
 
-	grid_del(grid);
+	/* Note: it's not actually possible to test this... need to wait for
+	 * 128bit processors come along!
+	 */
+
+	/*
+	 *grid = grid_new(UINT_MAX - 1, UINT_MAX - 1, 0);
+	 *grid_set(grid, coord(UINT_MAX - 2, UINT_MAX - 2), SQUARE_BOMB);
+	 *assert(square_is_bomb(grid_get(grid, coord(UINT_MAX - 2, UINT_MAX - 2))));
+	 */
 
 	grid = grid_new(16, 16, 0);
 	grid_set(grid, coord(16, 16), SQUARE_BOMB);
+
+	grid_del(grid);
 
 	return 0;
 }
