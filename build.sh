a7b036cbab075b6ce0f01da68224c934a40b667d
Corey Richardson <corey@octayn.net>
2012-11-11 20:25:03 -0500

    Ghetto build system v2

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
-gcc -g -o test_grid test_grid.c grid.c
-gcc -g -DNDEBUG -O1 -o bench_grid bench_grid.c grid.c
+if [ "$CC" = "" ] ; then
+	CC=gcc
+fi
+
+echo "$*" | grep debug > /dev/null
+
+if [ $? -eq 0 ]; then
+	CFLAGS+=-g
+fi
+
+$CC $CFLAGS -o test_grid test_grid.c grid.c
+$CC $CFLAGS -DNDEBUG -O1 -o bench_grid bench_grid.c grid.c
